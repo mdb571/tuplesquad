140f1157f817e85203aeffa5962f68a4654ba1cc
Akash <akashknair01998@gmail.com>
2021-11-23 15:07:17 +0530

    browser apis

--- source/ContentScript/index.js
+++ source/ContentScript/index.js
@@ -1,3 +1,15 @@
+/* eslint-disable prettier/prettier */
+import browser from 'webextension-polyfill';
+
 console.log('helloworld from content script');
 
-export {};
+browser.storage.local
+  .set({
+    [window.location.hostname]: document.title,
+  })
+  .then(() => {
+    browser.runtime.sendMessage(
+      `Saved document title for ${window.location.hostname}`
+    );
+  });
+export { };

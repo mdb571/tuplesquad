c081681b0fb7c796f4d9aca1094908864e0bae03
midhun1114 <71030184+midhun1114@users.noreply.github.com>
2021-11-24 00:23:50 +0530

    routes added

--- api/server.py
+++ api/server.py
@@ -1,56 +1,82 @@
 from flask import Flask, jsonify, request
-from flask.ext.pymongo import PyMongo
+from flask_pymongo import PyMongo
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from flask import jsonify, request
+from deta import Deta
+import json
+import pymongo
 
 app = Flask(__name__)
+deta = Deta('c052t59b_4GGFLneztbAy6QQLfkSV1C74uuPQVD4K')
+# app.config['MONGO_DBNAME'] = 'greenalt'
+# app.config['MONGO_URI'] = 'mongodb+srv://tuplesquad:tuplesquad7@cluster0.co0xz.mongodb.net/greenalt'
 
-app.config['MONGO_DBNAME'] = 'midhun'
-app.config['MONGO_URI'] = 'mongodb+srv://midhun1114:midhun1114@cluster0.co0xz.mongodb.net'
+# mongo = PyMongo(app)
 
-mongo = PyMongo(app)
-client = pymongo.MongoClient("mongodb+srv://midhun1114:midhun1114@cluster0.co0xz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = client.midhun
-collection=db['test']
-post1 ={"_id" :0,"item": "chair" ,"alt_url":"hjh"}
-post2 ={"_id" :1, "item": "table" ,"alt_url":"ghgh"}
-collection.insert_many([post1,post2])
+shopping = deta.Base("shopping")
 
-@app.route('/framework', methods=['GET'])
-def get_all_frameworks():
-    framework = mongo.db.framework
 
-    output = []
+# client = pymongo.MongoClient("mongodb+srv://midhun1114:midhun1114@cluster0.co0xz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+# db = client.midhun
+# collection=db['test']
 
-    for q in framework.find():
-        output.append({'item' : q['item'], 'alt_url' : q['alt_url']})
-
-    return jsonify({'result' : output})
-
-@app.route('/framework/<name>', methods=['GET'])
-def get_one_framework(item):
-    framework = mongo.db.framework
-
-    q = framework.find_one({'item' : item})
-
-    if q:
-        output = {'item' : q['item'], 'item' : q['item']}
+@app.route('/add', methods=['POST'])
+def add_alt():
+    __json = json.loads(request.data)
+    _item = __json['item']
+    _alt_url = __json['alt_url']
+    _alt_title = __json['alt_title']
+    _alt_img = __json['alt_img']
+    _alt_price = __json['alt_price']
+    if _item and _alt_price and _alt_img and _alt_url and _alt_title and request.method == 'POST':
+        # id=mongo.db.greentest.insert_one({"item":_item,'alt_title':_alt_title,"alt_url":_alt_url,"alt_price":_alt_price,"alt_img":_alt_img})
+        shopping.insert(
+            {"item": _item, 'alt_title': _alt_title, "alt_url": _alt_url, "alt_price": _alt_price, "alt_img": _alt_img})
+        resp = jsonify("added")
+        resp.status_code = 200
+        return resp
     else:
-        output = 'No results found'
+        return not_found()
 
-    return jsonify({'result' : output})
 
-@app.route('/framework', methods=['POST'])
-def add_framework():
-    framework = mongo.db.framework
+@app.route('/shopping', methods=['POST'])
+def get_one_framework():
+    title = json.loads(request.data)
+    title = title["title"].lower()
+    title = title.split()
+    print(title)
+    ourItems = ['chair','pen']
+    found =[]
+    for i in title:
+        if i in ourItems:
+            found.append(i)
 
-    item = request.json['item']
-    alt_url = request.json['alt_url']
+    name = found[0]
+    res = shopping.fetch({
+        "item?contains": name
+    })
+    all_items = res.items
 
-    framework_id = framework.insert({'item' : item, 'alt_url' : alt_url})
-    new_framework = framework.find_one({'_id' : framework_id})
+    # fetch until last is 'None'
+    while res.last:
+        res = shopping.fetch({
+            "item?contains": name
+        }, last=res.last)
+        all_items += res.items
+    return jsonify(all_items)
 
-    output = {'item' : new_framework['item'], 'alt_url' : new_framework['alt_url']}
 
-    return jsonify({'result' : output})
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': "Not Found" + request.url
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
